682d580846f1d18724afe18c890a6e3c88198912
Alex <alextheprogrammer21@gmail.com>
2020-01-19 19:05:29 +0000

    ice cream stuff

---
+++ ice-cream.js
@@ -0,0 +1,10 @@
+// Do not modify these first two lines
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours); //prints the entire array 
+console.log(iceCreamFlavours[0]); //prints out the first flavour
+console.log(iceCreamFlavours[5]); //prints out the last flavour
+console.log(iceCreamFlavours.length); //prints out how many flavours there are
